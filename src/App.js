b0ac055cf6c04a3ac8480ae58f686e96156069e3
Sheeva Kavousian <sheevakavous@gmail.com>
2022-01-13 21:40:07 +0000

    Add destination components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react"
+import Destination from "./components/Destination"
 import Navbar from "./components/Navbar"
+import data from "./data"
 
 export default function App() {
+
+    const destinationCard = data.map(item => {
+        return (
+            <Destination
+                key = {item.id}
+                {...item}
+            />
+        )
+    })
+
     return(
-        <Navbar />
+        <div>
+            <Navbar /> 
+            <section className="destinations">
+                {destinationCard}
+            </section>
+        </div>
+        
     )
 }
--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,12 +1,24 @@
 import React from "react"
+import Pin from "../Images/Pin.png"
 
 export default function Destination(props) {
 
     return (
-        <section>
-            <img />
-            <img />
-            <h5>{props}</h5>
-        </section>
+        <article>
+            <img src={require("../Images/" + props.image)}/>
+            <div className="destination--container">
+                <div className="destination--info">
+                    <img className="pin" src={Pin}/>
+                    <h5 className="destination--location">{props.location}</h5>
+                    <a href={props.googleMapsUrl}>View on Google Maps</a>
+                </div>
+                <h2 className="destination--title">{props.title}</h2>
+                <h5 className="destination--date">{props.startDate} - {props.endDate}</h5>
+                <p className="destination--desc">{props.description}</p>
+            </div>
+            
+            
+            
+        </article>
     )
 }
